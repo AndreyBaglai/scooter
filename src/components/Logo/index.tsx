5c9bb14097bd378cae99d4a0d4057d1cff14383e
AndreyBaglai <andreybaglai@gmail.com>
2021-10-19 15:20:12 +0300

    refactor: logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ReactComponent as Logo } from 'sources/icons/logo.svg';
+
+const _Logo: React.FC = () => {
+  return <Logo />
+};
+
+export default _Logo;
--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import Button from 'components/Button';
 import Header from 'components/Header';
 import Navigation from 'components/Navigation';
-
-import { ReactComponent as Logo } from 'sources/icons/logo.svg';
+import Logo from 'components/Logo';
 
 const App: React.FC = () => {
   return (
