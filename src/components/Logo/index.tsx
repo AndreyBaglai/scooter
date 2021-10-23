b97e8e690e6bbef47d2280f9cf476d23e6b34629
AndreyBaglai <andreybaglai@gmail.com>
2021-10-23 15:21:10 +0300

    fix: styles on mobile devices

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 import { ReactComponent as Logo } from 'sources/icons/logo.svg';
 
-const _Logo: React.FC = () => {
-  return <Logo />
+import styles from './styles.module.scss';
+import cn from 'classnames';
+
+interface IProps {
+  isOrange?: boolean;
+}
+
+const _Logo: React.FC<IProps> = ({ isOrange }) => {
+  return <Logo className={cn(styles.logo, { [styles.orange]: isOrange })} />
 };
 
 export default _Logo;
