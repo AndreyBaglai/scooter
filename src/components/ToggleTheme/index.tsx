f482fca94a0665ab56a288810d5ea54d30768585
AndreyBaglai <andreybaglai@gmail.com>
2021-10-25 10:50:39 +0300

    feat: add animation for toggle theme component

--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -6,8 +6,8 @@ import { useStore } from 'stores';
 import styles from './styles.module.scss';
 import cn from 'classnames';
 
-const getToggleIcons = (): HTMLElement[] =>
-  Array.from(document.querySelectorAll('span[data-icon="icon"]'));
+const getElementsByDataAttr = (dataAttr: string): HTMLElement[] =>
+  Array.from(document.querySelectorAll(`span[${dataAttr}]`));
 
 const ToggleTheme: React.FC = observer(() => {
   const { themeStore } = useStore();
@@ -15,35 +15,49 @@ const ToggleTheme: React.FC = observer(() => {
 
   useEffect(() => {
     const themeFromLS = localStorage.getItem('theme');
-    themeFromLS && themeStore.changeTheme(themeFromLS);
+    // themeFromLS && themeStore.changeTheme(themeFromLS);
 
-    if (themeFromLS === 'dark') {
-      document.body.classList.add('dark');
+    // if (themeFromLS === 'dark') {
+    //   document.body.classList.add('dark');
 
-      const toggleIcons: HTMLElement[] = getToggleIcons();
-      toggleIcons.forEach((icon: HTMLElement) => (icon.style.left = '8px'));
-    }
+    //   // const toggleThumbs: HTMLElement[] = getToggleThumbs();
+    //   // toggleThumbs.forEach((thumb: HTMLElement) => (thumb.style.left = '8px'));
+    // }
   }, [theme]);
 
   const onToggleTheme = () => {
-    const toggleIcons: HTMLElement[] = getToggleIcons();
+    const toggleThumbs: HTMLElement[] = getElementsByDataAttr('data-thumb="thumb"');
+    const lightTextElements = getElementsByDataAttr('data-light="light"');
+    const darkTextElements = getElementsByDataAttr('data-dark="dark"');
+
     document.body.classList.toggle('dark');
 
     if (theme === 'light') {
       themeStore.changeTheme('dark');
       localStorage.setItem('theme', 'dark');
-      toggleIcons.forEach((icon: HTMLElement) => (icon.style.left = '8px'));
+      toggleThumbs.forEach((thumb: HTMLElement) => (thumb.style.right = '0'));
+
+      lightTextElements.forEach((el: HTMLElement) => (el.style.color = '#f1f1f1'));
+      darkTextElements.forEach((el: HTMLElement) => (el.style.color = '#000'));
     } else {
       themeStore.changeTheme('light');
       localStorage.setItem('theme', 'light');
-      toggleIcons.forEach((icon: HTMLElement) => (icon.style.left = '49px'));
+      toggleThumbs.forEach((thumb: HTMLElement) => (thumb.style.right = '115px'));
+
+      lightTextElements.forEach((el: HTMLElement) => (el.style.color = '#000'));
+      darkTextElements.forEach((el: HTMLElement) => (el.style.color = '#f1f1f1'));
     }
   };
 
   return (
     <div className={styles.toggle} onClick={onToggleTheme}>
-      <span className={styles.white}>Белый</span>
-      <span className={cn(styles.black, styles.active)}>Черный</span>
+      <span className={styles.thumb} data-thumb="thumb"></span>
+      <span className={styles.light} data-light="light">
+        Белый
+      </span>
+      <span className={styles.dark} data-dark="dark">
+        Черный
+      </span>
     </div>
   );
 });
